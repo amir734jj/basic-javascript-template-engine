962804d42088b65e15d023a76cfcd50693ab9d79
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2017-06-18 12:57:39 -0500

    added test case

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # basic-javascript-template
 Basic javascript template system
+
+    (function() {
+        var self = this;
+        something = "something else!";
+        var template = "\
+                        <h2 class='{{= (true).toString()}}'>{{0}} {{1}} {{2}}</h2>\
+                        <p>{{= 4 > 0 ? 'good' : 'bad'}}</p>\
+                        <p>{{= something }}</p>\
+                        <p>{{name}}</p>\
+                    ";
+
+        var x = new amirTemplateEngine();
+
+        console.log(x.format(template, {
+            array: ["seyed", "amir", "hossein"],
+            values: {
+                name: "amir"
+            },
+            context: self
+        }));
+    })();
---
+++ test-amir-template-engine.js
@@ -0,0 +1,31 @@
+var colors = require('./amir-template-engine');
+
+(function() {
+    var self = this;
+    something = "something else!";
+    var template = "\
+                    <h2 class='{{= (true).toString()}}'>{{0}} {{1}} {{2}}</h2>\
+                    <p>{{= 4 > 0 ? 'good' : 'bad'}}</p>\
+                    <p>{{= something }}</p>\
+                    <p>{{name}}</p>\
+                ";
+
+    var result = "\
+                    <h2 class='true'>seyed amir hossein</h2>\
+                    <p>good</p>\
+                    <p>something else!</p>\
+                    <p>amir</p>\
+                ";
+
+    var templateObj = new amirTemplateEngine();
+
+    testResult = templateObj.format(template, {
+        array: ["seyed", "amir", "hossein"],
+        values: {
+            name: "amir"
+        },
+        context: self
+    }) == result;
+
+    console.log("Test is" + (testResult ? "" : " NOT") + " passing!");
+})();
